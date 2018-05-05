4774f91bd1bd2c5fb45fab044888772afd9d8613
Md. Jahed Bin Jalil <jahed_jony@yahoo.com>
2018-05-05 17:32:05 +0600

    category wise page

---
+++ application/views/category_blog.php
@@ -0,0 +1,18 @@
+<?php 
+foreach($category_info as $c_blog){
+?>
+<div class="article">
+  <h2><span><?php echo $c_blog->blog_title;?></h2>
+  <p class="infopost">Posted by <a href="#"><?php echo $c_blog->author_name;?></a> &nbsp;&nbsp;|&nbsp;&nbsp; Filed under <a href="#"><?php echo $c_blog->category_name;?></a> <a href="#" class="com">Comments <span>11</span></a></p>
+  <div class="clr"></div>
+  <div class="img"><img src="<?php echo base_url();?>images/img1.jpg" width="177" height="213" alt="" class="fl" /></div>
+  <div class="post_content">
+	<p><?php echo $c_blog->blog_short_description;?></p>
+	<p class="spec"><a href="<?php echo base_url(); ?>/welcome/blog_details/<?php echo $c_blog->blog_id; ?>" class="rm">Read more &raquo;</a></p>
+  </div>
+  <div class="clr"></div>
+</div>
+<?php 
+}
+?>
+<p class="pages"><small>Page 1 of 2</small> <span>1</span> <a href="#">2</a> <a href="#">&raquo;</a></p>
