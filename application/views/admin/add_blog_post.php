d98774108a61d78c04f65c917fe49ad735ed5393
Md. Jahed Bin Jalil <jahed_jony@yahoo.com>
2018-05-05 15:39:57 +0600

    blog post form

---
+++ application/views/admin/add_blog_post.php
@@ -0,0 +1,46 @@
+<div class="box-content">
+	<form class="form-horizontal" id="edit_category_form" name="edit_category_form" action="<?php echo base_url(); ?>super_admin/save_category" method="post">
+		<fieldset>
+			<legend>Edit Category</legend>
+			<h3 style="color:green">
+				<?php
+					$msg=$this->session->userdata('message');
+					if($msg){
+						echo $msg;
+						$this->session->unset_userdata('message');
+					}
+				?>
+			</h3>
+			<div class="control-group">
+				<label class="control-label" for="typeahead">Category Name <em style="color:red">*</em></label>
+				<div class="controls">
+					<input type="text" class="span6 typeahead" name="category_name" id="category_name" value="<?php echo $category_info->category_name; ?>" required>
+				</div>
+			</div>
+			<div class="control-group">
+				<label class="control-label" for="textarea2">Description</label>
+				<div class="controls">
+					<textarea class="cleditor" name="category_description" id="textarea2" rows="3"><?php echo $category_info->category_description; ?></textarea>
+				</div>
+			</div>
+			<div class="control-group">
+				<label class="control-label" for="textarea2">Publication Status</label>
+				<div class="controls">
+					<select name="status">
+						<option>Select Option</option>
+						<option value="1">Published</option>
+						<option value="0">UnPublished</option>
+					</select>
+				</div>
+			</div>
+			<div class="form-actions">
+				<button type="submit" class="btn btn-primary">Update</button>
+				<button type="reset" class="btn">Cancel</button>
+			</div>
+		</fieldset>
+	</form>
+</div>
+
+<script>
+	document.forms['edit_category_form'].elements["status"].value='<?php echo $category_info->publication_status ?>';
+</script>
