516a4e4133377f88a81814168c1afd8deaa713b5
Md. Jahed Bin Jalil <jahed_jony@yahoo.com>
2018-05-06 16:11:46 +0600

    error correction

--- application/models/welcome_model.php
+++ application/models/welcome_model.php
@@ -14,10 +14,12 @@ class Welcome_model extends CI_Model {
 	}
 	
 	public function all_published_blog(){
-		$this->db->select('*');
+		//$this->db->select('*');
+		$this->db->select('tbl_blog.*,tbl_category.category_name,tbl_category.category_id');
 		$this->db->from('tbl_blog');
 		$this->db->join('tbl_category', 'tbl_category.category_id = tbl_blog.category_id', 'left');
 		$this->db->where('tbl_blog.publication_status',1);
+		$this->db->where('tbl_category.publication_status', 1);
 
 		$query_result = $this->db->get();
 		$result = $query_result->result();
@@ -25,7 +27,8 @@ class Welcome_model extends CI_Model {
 	}
 	
 	public function select_blog_info_by_id($blog_id){
-		$this->db->select('*');
+		//$this->db->select('*');
+		$this->db->select('tbl_blog.*,tbl_category.category_name,tbl_category.category_id');
 		$this->db->from('tbl_blog');
 		$this->db->join('tbl_category', 'tbl_category.category_id = tbl_blog.category_id', 'left');
 		$this->db->where('tbl_blog.blog_id',$blog_id);
@@ -35,10 +38,12 @@ class Welcome_model extends CI_Model {
 	}
 	
 	public function select_blog_info_by_category_id($category_id){
-		$this->db->select('*');
+		//$this->db->select('*');
+		$this->db->select('tbl_blog.*,tbl_category.category_name,tbl_category.category_id');
 		$this->db->from('tbl_blog');
 		$this->db->join('tbl_category', 'tbl_category.category_id = tbl_blog.category_id', 'left');
 		$this->db->where('tbl_blog.category_id',$category_id);
+		$this->db->where('tbl_blog.publication_status',1);
 		$query_result = $this->db->get();
 		$result = $query_result->result();
 		return $result;
